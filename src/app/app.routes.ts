7f07130cebd2b06ef1746356a9da486f63ec24d3
Sebastien Peltier <sebpel62@gmail.com>
2024-05-14 10:30:33 +0200

    router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserProfilComponent } from './user-profil/user-profil.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: UserProfilComponent },
+
+{ path: 'signup', component: SignUpComponent },
+{ path: 'user', component: UserProfilComponent },
+];
